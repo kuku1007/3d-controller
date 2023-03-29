f09bfba8bab308b62acb1d226e86cb898630723c
kuku1007 <lukasz.maslow@gmail.com>
2023-03-29 09:20:18 +0200

    slippery-8rays, fall trigger checks y velocity

--- Assets/Scripts/State/FallState.cs
+++ Assets/Scripts/State/FallState.cs
@@ -12,16 +12,15 @@ namespace LM
             Debug.Log("Entering Fall State");
             playerLocomotion.currentInAirDirection = playerLocomotion.inputDirection;
             playerLocomotion.movementSpeed = 3;
-            playerLocomotion.HandleSlippery();
             playerLocomotion.playerAnimationManager.PlayTargetAnimation("Falling", true);
         }
 
         public override void OnUpdate(PlayerLocomotion playerLocomotion)
         {
-            playerLocomotion.HandleSlippery();
             playerLocomotion.HandleMovement(playerLocomotion.currentInAirDirection);
             playerLocomotion.HandleGravity();
-            if(playerLocomotion.isOnGround) ExitState(playerLocomotion, playerLocomotion.moveState);  
+            playerLocomotion.HandleAimRotationOnly();
+            if(playerLocomotion.isOnGround || playerLocomotion.characterVelocity.y == 0) ExitState(playerLocomotion, playerLocomotion.moveState);  
         }
 
         public override void ExitState(PlayerLocomotion playerLocomotion, State newState)
